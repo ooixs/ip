35e2f85a3453c076151055d1b69942155c2be710
Xuan Shan <42229271+ooixs@users.noreply.github.com>
2026-08-19 23:07:25 +0800

    rename chatbot and add greeting

---
+++ src/main/java/NotMarth.java
@@ -0,0 +1,21 @@
+/**
+ * The main entry point for the NotMarth chatbot.
+ */
+public class NotMarth {
+    public static void main(String[] args) {
+        String separator = "_".repeat(60);
+        String banner = " _   _  ___ _____ __  __    _    ____ _____ _   _\n"
+                + "| \\ | |/ _ \\_   _|  \\/  |  / \\  |  _ \\_   _| | | |\n"
+                + "|  \\| | | | || | | |\\/| | / _ \\ | |_) || | | |_| |\n"
+                + "| |\\  | |_| || | | |  | |/ ___ \\|  _ < | | |  _  |\n"
+                + "|_| \\_|\\___/ |_| |_|  |_/_/   \\_\\_| \\_\\_| |_| |_|\n";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm NotMarth.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
