ac94af6bb7c543e0e3c9f6122113b0c40e6b1de6
Xuan Shan <42229271+ooixs@users.noreply.github.com>
2026-08-19 23:19:00 +0800

    update code to echo commands entered by user

--- src/main/java/NotMarth.java
+++ src/main/java/NotMarth.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The main entry point for the NotMarth chatbot.
  */
@@ -15,7 +17,21 @@ public class NotMarth {
         System.out.println("Hello! I'm NotMarth.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(separator);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(separator);
+            System.out.println("     " + command);
+            System.out.println(separator);
+        }
     }
 }
